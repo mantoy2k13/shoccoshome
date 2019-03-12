89abdd7cd6fc5e9b5bb7f9c5e5275695ed842551
MJ TBL <spymyway6@gmail.com>
2019-03-12 23:35:39 +0800

    Fixed display issues and new data from added from database, Fixed small UI issues, display new pet added on the pet details, optimize update pets data, Pet Pictures must have and option to select from device or from photos

--- application/controllers/Pet.php
+++ application/controllers/Pet.php
@@ -67,148 +67,6 @@ class Pet extends CI_Controller{
         }
     }
 
-    public function update_this_pet(){
-        $petid=$this->input->post('pet_id');
-
-        $image_name = [];
-        $uploadedImgs=$this->input->post('pet_images');
-
-        $upimagecheck = ($uploadedImgs) ? count($uploadedImgs) : 0;
-        
-        define('UPLOAD_DIR', 'assets/img/pet/');
-        for($i = 0; $i < $upimagecheck; $i++){
-            $img_name = uniqid().'.png';
-            $aa=$uploadedImgs[$i];
-            $image_parts = explode(";base64,", $aa);
-            $image_type_aux = explode("image/", $image_parts[0]);
-            $image_type = $image_type_aux[1];
-            $image_base64 = base64_decode($image_parts[1]);
-            $file = UPLOAD_DIR . $img_name;
-            file_put_contents($file, $image_base64);
-            $image_name[$i]=$file;
-        }
-
-        //print_r($image_name);
-        $get_single_pet_data=$this->Pet_model->get_single_pet_data($petid);
-        $get_pet_images=$get_single_pet_data[0]->pet_images;
-
-        $only_pet_image= json_decode($get_pet_images);
-        $final_images = array_merge($only_pet_image, $image_name);
-        $count_final_images=count($final_images);
-            if($count_final_images>4){
-                $petid=$this->input->post('pet_id');
-                $this->session->set_flashdata('update_over_img_success_msg', 'Sorry ! You Max 4 images Uploaded.');
-                redirect('pet/add_new_pet/'.$petid.'');
-            }else{
-
-            $uploadedImgsJson=json_encode($final_images);
-            //exit();
-
-            $petdataupdate=array(
-                'user_id'=>$this->input->post('user_id'),
-                'pet_id'=>$this->input->post('pet_id'),
-                'pet_name'=>$this->input->post('pet_name'),
-                'cat_id'=>$this->input->post('cat_id'),
-                'breed_id'=>$this->input->post('breed_id'),
-                'tags'=>$this->input->post('tags'),
-                'gender'=>$this->input->post('gender'),
-                'color_id'=>$this->input->post('color_id'),
-                'height'=>$this->input->post('height'),
-                'weight'=>$this->input->post('weight'),
-                'dob'=>$this->input->post('dob'),
-                'fav_food'=>$this->input->post('fav_food'),
-                'skills'=>$this->input->post('skills'),
-                'vet_clinic'=>$this->input->post('vet_clinic'),
-                'located'=>$this->input->post('located'),
-                'adoptable'=>$this->input->post('adoptable'),
-                'health_issues'=>$this->input->post('health_issues'),
-			    'medications'=>$this->input->post('medications'),
-                'description'=>$this->input->post('description'),
-                'country'=>$this->input->post('country'),
-                'state'=>$this->input->post('state'),
-                'city'=>$this->input->post('city'),
-                'street'=>$this->input->post('street'),
-                'zip_code'=>$this->input->post('zip_code'),
-                'vaccination'=>$this->input->post('vaccination'),
-			    'vaccination_date'=>$this->input->post('vaccination_date'),
-                'activate_notice'=>$this->input->post('activate_notice'),
-                'notice_title'=>$this->input->post('notice_title'),
-                'chip_no'=>$this->input->post('chip_no'),
-                'collar_tag'=>$this->input->post('collar_tag'),
-                'reward'=>$this->input->post('reward'),
-                'lost_location'=>$this->input->post('lost_location'),
-                'lost_date'=>$this->input->post('lost_date'),
-                'other_info'=>$this->input->post('other_info'),
-                'contact_info'=>$this->input->post('contact_info'),
-                'alt_contact_info'=>$this->input->post('alt_contact_info'),
-                'pet_images'=>$uploadedImgsJson,
-            );
-            $update_pet = $this->Pet_model->updatepetdata($petdataupdate);
-            if ($update_pet) {
-                $this->session->set_flashdata('pet_msg', 'Updated');
-                redirect('pet/add_new_pet/'.$petid.'');
-            }
-            else {
-                $this->session->set_flashdata('pet_msg', 'Error');
-                redirect('pet/add_new_pet/'.$petid.'');
-            }
-
-        }
-    }
-
-    private function upload_files($path, $title, $files)
-    {
-        $config = array(
-            'upload_path'   => $path,
-            'allowed_types' => 'jpg|gif|png',
-            'overwrite'     => 1,
-        );
-    
-        $this->load->library('upload', $config);
-    
-        $images = array();
-         $uploadedFilename = array();
-    
-       foreach ($files['name'] as $key => $image) {
-            if(!empty($image)){
-                
-                $_FILES['images_file[]']['name']= $files['name'][$key];
-                $_FILES['images_file[]']['type']= $files['type'][$key];
-                $_FILES['images_file[]']['tmp_name']= $files['tmp_name'][$key];
-                $_FILES['images_file[]']['error']= $files['error'][$key];
-                $_FILES['images_file[]']['size']= $files['size'][$key];
-    
-                $fileName = $title .'_'. $image;
-    
-                $images[] = $fileName;
-    
-                $config['file_name'] = $fileName;
-    
-                $this->upload->initialize($config);
-    
-                if ($this->upload->do_upload('images_file[]')) {
-                    $this->upload->data();
-                    array_push($uploadedFilename, $this->upload->data('file_name'));
-                } else {
-                    return $this->upload->display_errors();
-                }
-            }
-       }
-       return $uploadedFilename;
-    }
-
-    private function set_upload_options()
-    {   
-        //upload an image options
-        $config = array();
-        $config['upload_path'] = './upload/pet/';
-        $config['allowed_types'] = 'gif|jpg|png';
-        $config['max_size']      = '0';
-        $config['overwrite']     = FALSE;
-
-        return $config;
-    }
-
     public function pet_image_remove($pet_id){
         if($this->input->post()){
             $pd = $this->Pet_model->get_single_pet_data($pet_id);
@@ -232,7 +90,6 @@ class Pet extends CI_Controller{
         }
     }
 
-
     public function categories_wise_breed_data(){
         
         if($this->input->post('categories')){
@@ -240,7 +97,6 @@ class Pet extends CI_Controller{
             $breeddata = $this->Pet_model->breed_data_dependancy_cat($categories_id);
             $allbreeddata = '<option value="">Select Breed</option>'; 
             foreach($breeddata as $breedlist){
-                //$statedatas[$statelist->id] = $statelist->name;
                 $allbreeddata = $allbreeddata . '<option value="'. $breedlist->breed_id .'">'.$breedlist->breed_name.'</option>'; 
             }		
             echo $allbreeddata;	
@@ -252,7 +108,6 @@ class Pet extends CI_Controller{
             $breeddata = $this->Pet_model->breed_data_dependancy_cat($cat_id);
             $allbreeddata = '<option value="">Select Breed</option>'; 
             foreach($breeddata as $breedlist){
-                //$statedatas[$statelist->id] = $statelist->name;
                 $allbreeddata = $allbreeddata . '<option value="'. $breedlist->breed_id .'">'.$breedlist->breed_name.'</option>'; 
             }		
             echo $allbreeddata;	
@@ -274,10 +129,10 @@ class Pet extends CI_Controller{
         $delete_pet = $this->Pet_model->deletepet($pet_id);
         if ($delete_pet) {
             $this->session->set_flashdata('pet_msg', 'Deleted');
-            redirect('/home/my_pets');
+            redirect('home/my_pets');
         }else {
             $this->session->set_flashdata('pet_msg', 'Error');
-            redirect('/home/my_pets');
+            redirect('home/my_pets');
         }
     }
 
